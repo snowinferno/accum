597a37f9397660d450a5ffb0f31d00b044ecfe61
Greg Wilburn <snowinferno@gmail.com>
2012-02-11 09:51:51 -0800

    Updated to use a custom event: 'complete'. Second argument now considered the function to be used as the event listener for 'complete'.  On branch master  Changes to be committed: 	modified:   accum.js 	modified:   test.js

--- accum.js
+++ accum.js
@@ -1,19 +1,23 @@
-exports.createAccum = function( props, cb ){
-	var obj = {} // create a blank object with no properties so we can fill it with just the properties we need
-	obj.cb = cb; // store a reference to the callback
-	obj.propList = props; // store a list of properties that were set to check with the finalize method
-	obj.isFinished = false;
+var util = require( 'util' ),
+	events = require( 'events' );
+
+function Accum( props, cb ){
+	this.cb = cb; // store a reference to the callback
+	this.__propList = props; // store a list of properties that were set to check with the finalize method
+	this.isFinished = false;
 
 	// add a property to a new object for each property passed in the array props
 	for( var i = 0; i < props.length; i++ ){
-		obj[props[i]] = undefined; // define each property to be undefined explicitly
+		this[props[i]] = undefined; // define each property to be undefined explicitly
 	}
 
-	obj.finalize = function(){
+	this.on( 'complete', cb );
+
+/*	this.finalize = function(){
 		// method to call each time one of these properties gets modified
 		var isFinished = true; // assume it is finished
 		for( var i = 0; i < this.propList.length; i++ ){
-			if( this[this.propList[i]] === undefined ){
+			if( this[this.__propList[i]] === undefined ){
 				isFinished = false;
 			}
 		}
@@ -21,6 +25,26 @@ exports.createAccum = function( props, cb ){
 		this.isFinished = isFinished;
 
 		if( this.cb && isFinished ){ this.cb( this ); } // if we're finished and a callback was specified, send the object on to the callback
-	}
-	return obj; // return the new accumulator object.
+	}*/
+	events.EventEmitter.call( this ); // call the parent constructor
 }
+
+util.inherits( Accum, events.EventEmitter ); // inherit the methods from EventEmitter
+
+Accum.prototype.add = function( key, data ){
+	this[key] = data;
+	var isFinished = true; // assume it is finished
+	for( var i = 0; i < this.__propList.length; i++ ){
+		if( this[this.__propList[i]] === undefined ){
+			isFinished = false;
+		}
+	}
+
+	this.isFinished = isFinished;
+
+	if( isFinished ){
+		this.emit( 'complete', this );
+	}
+}
+
+exports.Accum = Accum; // export the constructor
--- test.js
+++ test.js
@@ -4,11 +4,13 @@ var test = new acc.Accum( ['a', 'b'], function( data ){
 	console.log( "We're finished!" + this.a + "\t" + this.b );
 });
 
-test.a = 1;
-test.finalize();
-console.log( test );
-test.b = 3;
-console.log( test );
-test.finalize();
+test.on( 'complete', function(){
+	console.log( "Event 'Complete' fired.");
+	console.dir( arguments );
+})
 
+test.add( 'a', 1 );
+console.log( test );
+test.add( 'b', 3 );
+console.log( test );
 console.dir(test);
